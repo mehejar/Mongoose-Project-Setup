ee19db6c3f85a74bf55055e9ad27baddcbc68ab7
mehejar <abdalmehejar@gmail.com>
2024-12-03 16:45:50 +0600

    Add Pre hook for validate the semester is not exist

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,21 +1,26 @@
 import { model, Schema } from "mongoose";
 import { TAcademicSemester, TMonth } from "./academicSemester.interface";
+// import { escape } from "querystring";
 
-export const monthSchema = new Schema<TMonth>({
-    type: String,
-    enum: ["January"
-        , "February"
-        , "March"
-        , "April"
-        , "May"
-        , "June"
-        , "July"
-        , "August"
-        , "September"
-        , "October"
-        , "November"
-        , "December"]
-})
+export const monthSchema = new Schema<TMonth>(
+    {
+        type: String,
+        enum: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ]
+    }
+);
 
 export const academicSemesterSchema = new Schema<TAcademicSemester>({
     name: {
@@ -31,14 +36,58 @@ export const academicSemesterSchema = new Schema<TAcademicSemester>({
     year: {
         type: String, required: true
     },
-    startMonth: monthSchema,
-    endMonth: monthSchema,
+    startMonth: {
+        type: String,
+        enum: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ]
+    },
+    endMonth: {
+        type: String,
+        enum: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ]
+    },
 
 },
     {
         timestamps: true
     })
 
+academicSemesterSchema.pre('save', async function (next) {
+    const isAcademicSemesterExist = await Semester.findOne({
+        year: this.year,
+        name: this.name,
+    })
+
+    if (isAcademicSemesterExist) {
+        throw new Error('Semester is already Exist')
+    } else {
+        next()
+    }
+})
 
 export const Semester = model<TAcademicSemester>('Semester', academicSemesterSchema)
 
